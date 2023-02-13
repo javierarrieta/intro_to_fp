67b6423806e0cd637c96b146f0634edeca084ad8
Javier Arrieta <javierarrieta@google.com>
2023-02-13 12:46:54 +0000

    Improve Echo program

--- src/main/scala/fp/intro/EchoProgram.scala
+++ src/main/scala/fp/intro/EchoProgram.scala
@@ -1,15 +1,19 @@
 package fp.intro
 
+import cats.syntax.show._
 import cats.effect.{ExitCode, IO, IOApp}
 import cats.effect.std.Console
 
 object EchoProgram extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
 
-    val myConsole = Console.make[IO]
-    val deferredA = myConsole.readLine
+    val myConsole: Console[IO] = Console.make[IO]
+    val input: IO[String] = myConsole.readLine
 
-    val echo = deferredA.flatMap(myConsole.println)
+    val prompt: IO[Unit] = myConsole.print("Type your input: ")
+    val requestInput = prompt.flatMap(_ => input)
+
+    val echo = requestInput.flatMap(i => myConsole.println(show"You typed: $i"))
 
     echo.map(_ => ExitCode.Success)
   }
